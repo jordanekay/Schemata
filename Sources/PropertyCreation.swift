b8722d45a75a755f17907ba6d3765c23e5e46a7a
Jordan Kay <jordan@fleuronic.com>
2025-05-29 20:46:38 -0400

    Fix property creation

--- Sources/PropertyCreation.swift
+++ Sources/PropertyCreation.swift
@@ -98,7 +98,7 @@ public func <<- <Model, Children: Sequence & Schemata.Model>(
 ) -> Property<Model, Children> where Children.Element: Schemata.Model {
     return Property<Model, Children>(
         keyPath: lhs,
-        path: Children.Element.anySchema.properties(for: rhs).last!.path,
+        path: Children.Element.schemaName,
         type: .toMany(Children.self)
     )
 }
